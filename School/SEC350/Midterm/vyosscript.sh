6342f9a6c4995bc19b69b3743a6c7645ebcaf681
coolaspie <jake@jwhite.network>
2019-02-28 19:18:22 -0500

    Finished VYOS Script

--- School/SEC350/Midterm/vyosscript.sh
+++ School/SEC350/Midterm/vyosscript.sh
@@ -14,9 +14,9 @@ set interfaces ethernet eth0 description SEC350-WAN
 set interfaces ethernet eth1 description DMZ
 set interfaces ethernet eth2 description LAN
 delete interfaces ethernet eth0 address dhcp
-set interfaces ehternet eth0 address 10.0.17.125/24
-set interfaces ehternet eth1 address 172.16.50.2/29
-set interfaces ehternet eth2 address 172.16.150.2/24
+set interfaces ethernet eth0 address 10.0.17.125/24
+set interfaces ethernet eth1 address 172.16.50.2/29
+set interfaces ethernet eth2 address 172.16.150.2/24
 commit
 save
 ## Settings firewalls and zones
@@ -39,12 +39,6 @@ set zone-policy zone LAN from DMZ firewall name DMZ-to-LAN
 set zone-policy zone LAN from WAN firewall name WAN-to-LAN
 commit
 save
-## LAN to Web01
-for i in "action accept" "destination address 172.16.50.3" "destitnation port 80" "protocol tcp" "description 'Allow LAN access to WEB01'"
-do
-set firewall name LAN-to-DMZ rule 10 $i
-done
-
 ## Set connection establied
 for zone in "DMZ-to-WAN" "DMZ-to-LAN" "WAN-to-LAN"
 do
@@ -54,11 +48,28 @@ do
   done
 done
 
+## LAN to Web01
+for i in "action accept" "destination address 172.16.50.3" "destitnation port 80" "protocol tcp" "description 'Allow LAN access to WEB01'"
+do
+set firewall name LAN-to-DMZ rule 10 $i
+done
 
+## Logs to syslog01
+for i in "action accept" "destination address 172.16.150.7" "destitnation port 514" "protocol tcp_udp" "description 'Log from DMZ to syslog01"
+do
+set firewall name DMZ-to-LAN rule 10 $i
+done
 
+## WAN to web01
+for i in "description 'Port Forwarding HTTP WAN to Web01'" "destination port 80" "inbound-interface eth0" "protocol tcp" "translation address 172.16.50.3"
+do
+set nat destination rule 10 $i
+done
 
-
-
-
-
-
+##Nat Masquerade
+for i in "description 'NAT FROM LAN to WAN'" "outbound-interface eth0" "source address 172.16.150.0/24" "translation address masquerade"
+do
+set nat source rule 10 $i
+done
+commit
+save
