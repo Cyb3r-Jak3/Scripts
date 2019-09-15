2cd834a7782997b683c8d321d855cbacccba90a7
Jacob <jake@jwhite.network>
2019-09-15 11:44:44 -0400

    Fixed link in setup and spacing on service file

--- Linux/osquery/setup.sh
+++ Linux/osquery/setup.sh
@@ -4,7 +4,7 @@ yum install -y yum-utils
 yum-config-manager --add-repo https://pkg.osquery.io/rpm/osquery-s3-rpm.repo
 yum-config-manager --enable osquery-s3-rpm
 yum install -y osquery
-curl https://gitlab.com/jwhite1st/Scripts/raw/master/Linux/osquery/setup.sh -o /etc/systemd/system/fleetconnect.service
+curl https://gitlab.com/jwhite1st/Scripts/raw/master/Linux/osquery/fleetconnect.service -o /etc/systemd/system/fleetconnect.service
 
 systemctl daemon-reload
 systemctl start fleetconnect.service
