caf8d677bc24dcdd456a2844e54ddb2fecfa23b5
Jacob <jake@jwhite.network>
2019-09-15 12:45:13 -0400

    Allows for input to configure server cert

--- Linux/osquery/setup.sh
+++ Linux/osquery/setup.sh
@@ -5,7 +5,9 @@ yum-config-manager --add-repo https://pkg.osquery.io/rpm/osquery-s3-rpm.repo
 yum-config-manager --enable osquery-s3-rpm
 yum install -y osquery
 curl https://gitlab.com/jwhite1st/Scripts/raw/master/Linux/osquery/fleetconnect.service -o /etc/systemd/system/fleetconnect.service
-
+read -p "Please enter the hostname/ip of your fleet server. (Include port): " server 
+openssl s_client -showcerts -connect $server </dev/null 2>/dev/null|openssl x509 -outform PEM > /var/osquery/server.pem
+sed -i -e "s/oldip\b/$server/g" /etc/systemd/system/fleetconnect.service
 systemctl daemon-reload
 systemctl start fleetconnect.service
 systemctl enable fleetconnect.service
