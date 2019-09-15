7679fd05f5c745bb96f5e360c40e0ccb5bc969e1
Jacob <jake@jwhite.network>
2019-09-15 11:31:12 -0400

    More attempts

--- Linux/osquery/setup.sh
+++ Linux/osquery/setup.sh
@@ -1,37 +1,38 @@
+#!/bin/bash
 curl -L https://pkg.osquery.io/rpm/GPG -o /etc/pki/rpm-gpg/RPM-GPG-KEY-osquery
 yum install -y yum-utils
 yum-config-manager --add-repo https://pkg.osquery.io/rpm/osquery-s3-rpm.repo
 yum-config-manager --enable osquery-s3-rpm
 yum install -y osquery
-cat <<EOF > /etc/systemd/system/fleetconnect.service
+cat <<- _EOF_ 
+  [Unit]
+  Description=Koldie OSQuery
+  After=network.target
 
-[Unit]
-Description=Koldie OSQuery
-After=network.target
+  [Service]
+  ExecStart=/usr/bin/osqueryd \
+    --enroll_secret_path=/var/osquery/enroll_secret \
+    --tls_server_certs=/var/osquery/server.pem \
+    --tls_hostname=184.171.152.208:8080 \
+    --host_identifier==uuid \
+    --enroll_tls_endpoint=/api/v1/osquery/enroll \
+    --config_plugin=tls \
+    --config_tls_endpoint=/api/v1/osquery/config \
+    --config_refresh=10 \
+    --disable_distributed=false \
+    --distributed_plugin=tls \
+    --distributed_interval=3 \
+    --distributed_tls_max_attempts=3 \
+    --distributed_tls_read_endpoint=/api/v1/osquery/distributed/read \
+    --distributed_tls_write_endpoint=/api/v1/osquery/distributed/write \
+    --logger_plugin=tls \
+    --logger_tls_endpoint=/api/v1/osquery/log \
+    --logger_tls_period=10
 
-[Service]
-ExecStart=/usr/bin/osqueryd \
-  --enroll_secret_path=/var/osquery/enroll_secret \
-  --tls_server_certs=/var/osquery/server.pem \
-  --tls_hostname=184.171.152.208:8080 \
-  --host_identifier==uuid \
-  --enroll_tls_endpoint=/api/v1/osquery/enroll \
-  --config_plugin=tls \
-  --config_tls_endpoint=/api/v1/osquery/config \
-  --config_refresh=10 \
-  --disable_distributed=false \
-  --distributed_plugin=tls \
-  --distributed_interval=3 \
-  --distributed_tls_max_attempts=3 \
-  --distributed_tls_read_endpoint=/api/v1/osquery/distributed/read \
-  --distributed_tls_write_endpoint=/api/v1/osquery/distributed/write \
-  --logger_plugin=tls \
-  --logger_tls_endpoint=/api/v1/osquery/log \
-  --logger_tls_period=10
-
-[Install]
-WantedBy=multi-user.target
-EOF
+  [Install]
+  WantedBy=multi-user.target
+  _EOF_ 
+  > /etc/systemd/system/fleetconnect.service
 
 systemctl daemon-reload
 systemctl start fleetconnect.service
