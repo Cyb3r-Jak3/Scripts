6024516c0382e3d527914e4a561cdcabc73438d5
Jacob <jake@jwhite.network>
2019-09-15 15:54:47 -0400

    Working on the random string

--- Linux/fleet/setup.sh
+++ Linux/fleet/setup.sh
@@ -9,8 +9,9 @@ cp fleet/linux/fleet* /usr/bin/
 # Gets an installs mysql
 wget https://repo.mysql.com/mysql57-community-release-el7.rpm
 rpm -i mysql57-community-release-el7.rpm
-yum update
-yum install mysql-server
+yum update -y
+yum install -y mysql-server
+systemctl start mysqld
 #Sets up mysql
 password_match=`awk '/A temporary password is generated for/ {a=$0} END{ print a }' /var/log/mysqld.log | awk '{print $(NF)}'`
 mysql -u root -p$password_match "ALTER USER 'root'@'localhost' IDENTIFIED BY '$password_match';"
@@ -19,7 +20,7 @@ mysql -u root -p$password_match "CREATE DATABASE kolide;"
 
 #Installs redis
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-yum install redis
+yum install -y redis
 systemctl enable redis
 systemctl start redis
 
@@ -32,7 +33,7 @@ openssl req -new -key /tmp/server.key -out /tmp/server.csr
 openssl x509 -req -days 366 -in /tmp/server.csr -signkey /tmp/server.key -out /tmp/server.cert
 
 # Generates random string for auth_jwt_key
-$random_string = head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20
+$random_string=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
 #Gets Fleet service file
 curl https://gitlab.com/jwhite1st/Scripts/raw/master/Linux/fleet/fleet.service -o /etc/systemd/system/fleet.service
 
