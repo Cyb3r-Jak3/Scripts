d9e28d8ea4e878285d9a5d7eb7ff5cdf1299cc51
Jacob <jake@jwhite.network>
2019-09-15 20:08:37 -0400

    Mixing my syntax with PowerShell

--- Linux/fleet/setup.sh
+++ Linux/fleet/setup.sh
@@ -12,7 +12,7 @@ rpm -i mysql57-community-release-el7.rpm
 yum update -y
 yum install -y mysql-server
 systemctl start mysqld
-$random_password=$(< /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
+random_password=$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
 #Sets up mysql
 password_match=`awk '/A temporary password is generated for/ {a=$0} END{ print a }' /var/log/mysqld.log | awk '{print $(NF)}'`
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$random_password';" | mysql -u root -p$password_match  --connect-expired-password
@@ -26,7 +26,7 @@ systemctl enable redis
 systemctl start redis
 
 #Prepares the database
-/usr/bin/fleet prepare db --mysql_address=127.0.0.1:3306 --mysql_database=kolide --mysql_username=root --mysql_password=$password_match
+/usr/bin/fleet prepare db --mysql_address=127.0.0.1:3306 --mysql_database=kolide --mysql_username=root --mysql_password=$random_password
 
 # Generates certificates
 openssl genrsa -out /tmp/server.key 4096
@@ -34,7 +34,7 @@ openssl req -new -key /tmp/server.key -out /tmp/server.csr
 openssl x509 -req -days 366 -in /tmp/server.csr -signkey /tmp/server.key -out /tmp/server.cert
 
 # Generates random string for auth_jwt_key
-$random_string=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
+random_string=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20)
 #Gets Fleet service file
 curl https://gitlab.com/jwhite1st/Scripts/raw/master/Linux/fleet/fleet.service -o /etc/systemd/system/fleet.service
 
@@ -42,7 +42,7 @@ curl https://gitlab.com/jwhite1st/Scripts/raw/master/Linux/fleet/fleet.service -
 sed -i -e "s/BADSTRING\b/$random_string/g" /etc/systemd/system/fleet.service
 
 #Changes database password
-sed -i -e "s/toor\b/$password_match/g" /etc/systemd/system/fleet.service
+sed -i -e "s/toor\b/$random_password/g" /etc/systemd/system/fleet.service
 
 #Starts and enables fleet
 systemctl daemon-reload
